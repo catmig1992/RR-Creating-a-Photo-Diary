7ef18df2949192f99d5c733182bc5637e3e87bff
Catherine Miguel <catmig1992@gmail.com>
2022-12-07 23:55:36 -0800

    import koalas into index.js file

--- bin.js
+++ bin.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
-const open = require('open')
-const { exec, spawn } = require('child_process')
+const open = require("open");
+const { exec, spawn } = require("child_process");
 exec('npx webpack --mode="development"', () => {
-    spawn('npx', [ 'webpack', '--watch', '--mode="development"' ], { stdio: 'inherit' });
-    open('./public/index.html')
-    console.log('Watching for changes in "./src"')
-})
+  spawn("npx", ["webpack", "--watch", '--mode="development"'], {
+    stdio: "inherit",
+    shell: true,
+  });
+  open("./public/index.html");
+  console.log('Watching for changes in "./src"');
+});
 
-spawn('node', ['script.js'], {
-    env: {
-        NODE_ENV: 'production',
-        PATH: process.env.PATH
-    }
-})
+spawn("node", ["script.js"], {
+  env: {
+    NODE_ENV: "production",
+    PATH: process.env.PATH,
+  },
+});
